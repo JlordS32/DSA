3d132935821ef9213d9524c6032593a815330ea1
JlordS32 <jlordsty128@gmail.com>
2025-04-07 07:33:56 +1000

    save

--- assignment/hash.cpp
+++ assignment/hash.cpp
@@ -1,258 +1,240 @@
-#include <utility>
+ #include <utility>
 #include <list>
 #include "hash.hpp"
 
 const float INIT_MAXLOADFACTOR = 1;
 const int INIT_IDXSIZE = 0;
 
-HashSet::Iterator HashSet::begin()
-{
-   return this->_elements.begin();
+HashSet::Iterator HashSet::begin() {
+  return this->_elements.begin();
 }
 
-HashSet::Iterator HashSet::end()
-{
-   return this->_elements.end();
+HashSet::Iterator HashSet::end() {
+  return this->_elements.end();
 }
 
-HashSet::HashSet() : _buckets(sizes[INIT_IDXSIZE]),
-                     _numElements(0),
-                     _maxLoadFactor(INIT_MAXLOADFACTOR),
-                     _currentBucketCount(sizes[INIT_IDXSIZE]),
-                     _selectableSize(INIT_IDXSIZE)
-{
-}
-
-HashSet::HashSet(const HashSet &other)
-{
-   this->_buckets = other._buckets;
-   this->_maxLoadFactor = other._maxLoadFactor;
-   this->_currentBucketCount = other._currentBucketCount;
-   this->_numElements = other._numElements;
-   this->_selectableSize = other._selectableSize;
-}
-
-std::size_t HashSet::hash(int key, std::size_t bucketCount = 0) const
-{
-   if (bucketCount == 0)
-      bucketCount = this->_currentBucketCount;
-   return key % bucketCount;
-}
-
-HashSet &HashSet::operator=(HashSet other)
-{
-   this->_buckets = other._buckets;
-   this->_maxLoadFactor = other._maxLoadFactor;
-   this->_currentBucketCount = other._currentBucketCount;
-   this->_numElements = other._numElements;
-   this->_selectableSize = other._selectableSize;
-
-   return *this;
-}
-
-HashSet::~HashSet()
+HashSet::HashSet() :
+  _buckets(_sizes[INIT_IDXSIZE]), 
+  _numElements(0), 
+  _maxLoadFactor(INIT_MAXLOADFACTOR),
+  _currentBucketCount(_sizes[INIT_IDXSIZE]),
+  _selectableSize(INIT_IDXSIZE)
 {
 }
 
-void HashSet::insert(int key)
-{
-   // We avoid inserting for values that already exists
-   // on the set.
-   if (contains(key))
+HashSet::HashSet(const HashSet& other) {
+  this->_buckets = other._buckets;
+  this->_maxLoadFactor = other._maxLoadFactor;
+  this->_currentBucketCount = other._currentBucketCount;
+  this->_numElements = other._numElements;
+  this->_selectableSize = other._selectableSize;
+}
+
+// Note: Second parameter for rehash(). 
+std::size_t HashSet::hash(int key, std::size_t bucketCount = 0) const {
+  if (bucketCount == 0) 
+    bucketCount = this->_currentBucketCount;
+  return key % bucketCount;
+}
+
+HashSet& HashSet::operator=(HashSet other) {
+  this->_buckets = other._buckets;
+  this->_maxLoadFactor = other._maxLoadFactor;
+  this->_currentBucketCount = other._currentBucketCount;
+  this->_numElements = other._numElements;
+  this->_selectableSize = other._selectableSize;
+
+  return *this;
+}
+
+HashSet::~HashSet() {
+}
+
+void HashSet::insert(int key) {
+  // We avoid inserting for values that already exists 
+  // on the set.
+  if (contains(key)) return;
+
+  // Validate first if we should rehash before inserting element.
+  if (loadFactor() > this->_maxLoadFactor) {
+    rehash();
+  }
+
+  // Insert value into the element list first.
+  // And get an iterator for it. Since the _elements
+  // vector grows from the front, we get the value at the back.
+  this->_elements.push_back(key);
+  auto it = std::prev(this->_elements.end());
+
+  // Get bucket based on hash value.
+  std::size_t index = hash(key);
+  auto& bucket = this->_buckets[index];
+
+  // Push the iterator into that bucket.
+  bucket.push_back(it);
+  // Increment current number of elements.
+  this->_numElements++;
+}
+
+bool HashSet::contains(int key) const {
+  // Get bucket
+  std::size_t index = hash(key);
+  auto& bucket = this->_buckets[index];
+
+  // Iterate through the selected bucket
+  // and check if item exists.
+  for (auto it = bucket.begin(); it != bucket.end(); it++) {
+    if (*(*it) == key) {
+      return true;
+    }
+  }
+
+  // Return false for fallback.
+  return false;
+}
+
+HashSet::Iterator HashSet::find(int key) {
+  // Evaluate first if key exists, we return nothing otherwise.
+  if (!contains(key)) return this->_elements.end();
+
+  // Get bucket
+  std::size_t index = hash(key);
+  auto& bucket = this->_buckets[index];
+
+  // Loop through the bucket and find the value co
+  for (auto it = bucket.begin(); it != bucket.end(); it++){
+    if (*(*it) == key) {
+      return *it;
+    }
+  }
+
+  // We return nothing for fall back
+  return this->_elements.end();
+}
+
+void HashSet::erase(int key) {
+  // Return if value does not exist.
+  if (!contains(key)) return;
+
+  // Get bucket again.
+  std::size_t index = hash(key);
+  auto& bucket = this->_buckets[index];
+
+  // Iterate through the bucket
+  for (auto it = bucket.begin(); it != bucket.end(); it++) {
+    // If found, simply remove the item on the bucket
+    // and decrement the number of elements, then return.
+    if (*(*it) == key) {
+      this->_elements.erase(*it);
+      bucket.erase(it);
+      this->_numElements--;
       return;
-
-   // Rehash if load factor breaches _maxLoadFactor;
-   float currentLoadFactor = static_cast<float>(_numElements) / _currentBucketCount;
-   if (currentLoadFactor > _maxLoadFactor)
-   {
-      _selectableSize++;
-      int newSize = sizes[_selectableSize];
-      rehash(newSize);
-   }
-
-   // We insert the value first into our contiguous list.
-   _elements.push_back(key);
-   auto it = std::prev(_elements.end());
-
-   // Get bucket using hash value as the index.
-   std::size_t index = hash(key);
-   // Insert into the selected bucket.
-   _buckets[index].push_back(it);
-
-   // Increment the number of elements.
-   _numElements++;
+    }
+  }
 }
 
-bool HashSet::contains(int key) const
-{
-   // Get bucket using hash value as the index.
-   std::size_t index = hash(key);
-   auto &bucket = _buckets[index];
+HashSet::Iterator HashSet::erase(HashSet::Iterator it) {
+  // Return if the iterator not valid.
+  if (it == _elements.end()) return it;
 
-   // Iterate through the selected bucket
-   // and check if item exists.
-   for (const auto &it : bucket)
-   {
-      if (*it == key)
-      {
-         return true;
-      }
-   }
+  // Get bucket using the iterator
+  int key = *it;
+  std::size_t index = hash(key);
+  auto& bucket = this->_buckets[index];
 
-   // Return false for fallback.
-   return false;
+  // Loop through the bucket and find the corresponding
+  // value and erase.
+  for (auto it = bucket.begin(); it != bucket.end(); it++) {
+    if (*(*it) == key) {
+      bucket.erase(it);
+      break;
+    }
+  }
+
+  // After the iterator is gone on the bucket,
+  // we also remove from the _elements list
+  auto nextIt = this->_elements.erase(it);
+  
+  // Decrement the number of elements.
+  this->_numElements--;
+  
+  // Simply return the iterator.
+  return nextIt;
 }
 
-void HashSet::erase(int key)
-{
-   // Return if value does not exist.
-   if (!contains(key))
-      return;
+void HashSet::rehash() {
+  int newSize = this->_sizes[this->_selectableSize++];
+  
+  // Instantiate a new bucket of new size.
+  std::vector<std::list<Iterator>> newBuckets(newSize);
 
-   // Get bucket again.
-   std::size_t index = hash(key);
-   auto &bucket = _buckets[index];
-
-   // Iterate through the bucket
-   for (auto it = bucket.begin(); it != bucket.end(); it++)
-   {
-      // If found, simply remove the item on the bucket
-      // and decrement the number of elements, then return.
-      if (*(*it) == key)
-      {
-         _elements.erase(*it);
-         bucket.erase(it);
-         _numElements--;
-         break;
-      }
-   }
-
-   return;
+  // We simply copy the stuff over from the old buckets 
+  // to the new buckets by getting a new index for each
+  // items on the bucket.
+  for (auto it = _buckets.begin(); it != _buckets.end(); it++) {
+    for (auto& item : *it) {
+      std::size_t newIndex = hash(*item, newSize);
+      newBuckets[newIndex].push_back(item);
+    }
+  }
+  
+  // We then assign the value of newBackets
+  // to our member variables `_buckets`.
+  this->_buckets = std::move(newBuckets);
+  this->_currentBucketCount = newSize;
 }
 
-HashSet::Iterator HashSet::find(int key)
-{
-   if (!contains(key))
-      return _elements.end();
-
-   std::size_t index = hash(key);
-   auto &bucket = _buckets[index];
-
-   for (auto it = bucket.begin(); it != bucket.end(); it++)
-   {
-      if (*(*it) == key)
-      {
-         return *it;
-      }
-   }
-
-   return _elements.end();
+// Returns the number of elements.
+std::size_t HashSet::size() const {
+  return this->_numElements;
 }
 
-HashSet::Iterator HashSet::erase(HashSet::Iterator it)
-{
-   if (it == _elements.end())
-      return it;
-
-   int key = *it;
-
-   // Get bucket
-   std::size_t index = hash(key);
-   auto &bucket = _buckets[index];
-
-   // Iterate through the bucket
-   for (auto it = bucket.begin(); it != bucket.end(); it++)
-   {
-      // If found, simply remove the item on the bucket
-      // and decrement the number of elements, then return.
-      if (*(*it) == key)
-      {
-         bucket.erase(it);
-         break;
-      }
-   }
-
-   auto nextIt = _elements.erase(it);
-   _numElements--;
-   return nextIt;
-   
+// Check if number of elements is 0.
+bool HashSet::empty() const {
+  return this->_numElements == 0;
 }
 
-void HashSet::rehash(std::size_t newSize)
-{
-   // Instantiate a new bucket of new size.
-   std::vector<std::list<std::list<int>::iterator>> newBuckets(newSize);
-
-   // We simply copy the stuff over from the old buckets
-   // to the new buckets by getting a new index for each
-   // items on the bucket.
-   for (auto it = _buckets.begin(); it != _buckets.end(); it++)
-   {
-      if (!(*it).empty())
-      {
-         for (auto &item : *it)
-         {
-            std::size_t newIndex = hash(*item, newSize);
-            newBuckets[newIndex].push_back(item);
-         }
-      }
-   }
-
-   // We then assign the value of newBackets
-   // to our member variables `_buckets`.
-   this->_buckets = newBuckets;
-   this->_currentBucketCount = newSize;
+// Getter for _currentBucketCount.
+std::size_t HashSet::bucketCount() const {
+  return this->_currentBucketCount;
 }
 
-std::size_t HashSet::size() const
-{
-   return _numElements;
+// We simply return the .size() of the bucket,
+// presuming it exists. Most case if the b maps 
+// to empty bucket, it should return 0.
+std::size_t HashSet::bucketSize(std::size_t b) const {
+  auto& bucket = this->_buckets[b];
+  return bucket.size();
 }
 
-bool HashSet::empty() const
-{
-   return this->_numElements == 0;
+// Use hash function.
+std::size_t HashSet::bucket(int key) const {
+  return hash(key);
 }
 
-std::size_t HashSet::bucketCount() const
-{
-   return this->_currentBucketCount;
+// Get current load factor.
+float HashSet::loadFactor() const {
+  return static_cast<float>(this->_numElements) / this->_currentBucketCount;
 }
 
-std::size_t HashSet::bucketSize(std::size_t b) const
-{
-   auto &bucket = _buckets[b];
-   return bucket.size();
+// Get max load factor.
+float HashSet::maxLoadFactor() const {
+  return this->_maxLoadFactor;
 }
 
-std::size_t HashSet::bucket(int key) const
-{
-   return hash(key);
-}
 
-float HashSet::loadFactor() const
-{
-   return static_cast<float>(_numElements) / _currentBucketCount;
-}
+void HashSet::maxLoadFactor(float maxLoad) {
+  // Guard clause to ensure maxLoad does not go less than 0.
+  if (maxLoad < 0) maxLoad = 0.1;
 
-float HashSet::maxLoadFactor() const
-{
-   return _maxLoadFactor;
-}
+  // Assign the value to maxLoadFactor
+  this->_maxLoadFactor = maxLoad;
 
-void HashSet::maxLoadFactor(float maxLoad)
-{
-   if (maxLoad < 0)
-      maxLoad = 0.1;
-   this->_maxLoadFactor = maxLoad;
-
-   // Fallback in case the new load factor is less than the current load.
-   // Same implementation as in the member function insert().
-   float currentLoad = static_cast<float>(_numElements) / _currentBucketCount;
-   if (currentLoad > _maxLoadFactor)
-   {
-      _selectableSize++;
-      int newSize = sizes[_selectableSize];
-      rehash(newSize);
-   }
+  // We keep rehashing until currentLoad is less than the new max load factor.
+  while (loadFactor() > this->_maxLoadFactor) {
+    // Another guard clause to ensure we don't go over sizes[sizes.size() + 1].
+    // We get segmentation fault if we go over.
+    if (this->_selectableSize + 1 > this->_sizes.size()) break;
+    rehash();
+  }
 }
--- assignment/main.cpp
+++ assignment/main.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <cassert>
+#include <gtest/gtest.h>
 #include "hash.cpp"
+#include <random>
 
-int main()
-{
-   auto start = std::chrono::high_resolution_clock::now();
+// TEST(Level2Test, iteratorsValidAfterRehash)
+// {
+//    HashSet h;
+//    h.maxLoadFactor(1.0);
+//    std::vector<int> values{0, 13, 26, 39, 52, 65};
+//    for (int x : values)
+//    {
+//       h.insert(x);
+//    }
+//    auto it = h.find(26);
+//    ASSERT_EQ(*it, 26);
+//    for (int i = 0; i < 100; ++i)
+//    {
+//       h.insert(i);
+//    }
+//    ASSERT_EQ(*it, 26);
+// }
 
+// TEST(Level1Test, setLoadFactorProvokesRehash)
+// {
+//    HashSet h;
+//    ASSERT_TRUE(h.empty());
+//    float threshold = 1.0;
+//    h.maxLoadFactor(threshold);
+//    std::mt19937 mt{89'338'157};
+//    std::uniform_int_distribution<int> dist{-10'000, 10'000};
+//    for (int i = 0; i < 1'000; i++)
+//    {
+//       int elem = dist(mt);
+//       h.insert(elem);
+//    }
+//    ASSERT_FALSE(h.empty());
+//    std::size_t initialSize = h.size();
+//    threshold = 0.5;
+//    h.maxLoadFactor(threshold);
+//    ASSERT_GE(h.bucketCount(), 2 * initialSize);
+// }
+
+TEST(CollisionTest, SameBucketIndex) {
    HashSet hs;
-   // Set a low max load factor to force rehashing sooner.
-   
-   // Insert enough elements to trigger a rehash.
-   for (int i = 0; i < 50; ++i)
-   {
-      hs.insert(i);
-   }
-   
-   // Verify that all elements are still present.
-   for (int i = 0; i < 50; ++i)
-   {
-      assert(hs.contains(i));
-   }
-   
-   for (int x : hs) {
-      std::cout << x << " ";
-   }
-   
-   auto end = std::chrono::high_resolution_clock::now();
-   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+   // Set a high max load factor so that rehashing doesn't occur during this test.
+   hs.maxLoadFactor(1);
+   hs.insert(13);
+   hs.insert(26);
+   // Since 13 % bucketCount == 26 % bucketCount for a bucketCount like 13,
+   // they should fall in the same bucket.
+   ASSERT_EQ(hs.bucketSize(hs.bucket(13)), 2);
+   ASSERT_EQ(hs.bucket(13), hs.bucket(26));
+}
 
-   std::cout << "\n\nTime taken: " << duration.count() << " ms\n";
-   return 0;
+int main(int argc, char **argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
 }
