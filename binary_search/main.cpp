743918cd2b06350e0778018d2d9ee3cc53952875
JlordS32 <jlordsty128@gmail.com>
2025-04-13 14:37:20 +1000

    min heap done

--- binary_search/main.cpp
+++ binary_search/main.cpp
@@ -2,24 +2,36 @@
 #include <vector>
 #include <algorithm>
 
-int maxProfit(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+using Iterator = std::vector<int>::iterator;
+
+int maxProfit(Iterator begin, Iterator end)
 {
    if (end - begin <= 1)
    {
       return 0;
    }
 
-   std::vector<int>::iterator mid = begin + ((end - begin) / 2);
+   Iterator mid = begin + ((end - begin) / 2);
    int profit = *std::max_element(mid, end) - *std::min_element(begin, mid);
 
    return std::max({maxProfit(begin, mid), maxProfit(mid, end), profit});
 }
 
+int binarySearch(Iterator begin, Iterator end, int target) {
+   if (begin >= end) return -1;
+   Iterator mid = begin + ((end - begin) / 2);
+
+   if (*mid == target) return *mid;
+   else if (*mid > target) return binarySearch(begin, mid, target);
+   else return binarySearch(mid + 1, end, target);
+}
+
 int main()
 {
    std::vector<int> prices = {23, 5, 2, 64, 10, 1, 100, 23};
+   sort(prices.begin(), prices.end());
    
-   std::cout << maxProfit(prices.begin(), prices.end()) << std::endl;
+   std::cout << binarySearch(prices.begin(), prices.end(), 233) << std::endl;
 
    return 0;
 }
