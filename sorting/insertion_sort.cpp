2321f33815e98f75eba813f4412af4276b8d8a73
JlordS32 <jlordsty128@gmail.com>
2025-04-19 07:58:10 +1000

    idk what to say

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -1,29 +1,29 @@
 #include <vector>
 
-// void insertionSort(std::vector<T>& arr) {
-//    int i, key, j;
+void insertionSort(std::vector<int> & arr) {
+   int n = arr.size();
 
-//    for (int i = 1; i < arr.size(); i++) {
-//       key = arr[i];
-//       j = i - 1;
+   for (int i = 1; i < n; i++) {
+      int key = arr[i];
+      int j = i - 1;
 
-//       while (j >= 0 && arr[j] > key) {
-//          arr[j + 1] = arr[j];
-//          j--;
+      while (j >= 0 && arr[j] > key) {
+         arr[j + 1] = arr[j];
+         j--;
+      }
+
+      arr[j + 1] = key;
+   }
+}
+
+// void insertionSort(std::vector<int> &arr)
+// {
+//    for (std::size_t i = 1; i < arr.size(); i++)
+//    {
+//       while (i >= 1 && arr[i] < arr[i - 1])
+//       {
+//          std::swap(arr[i], arr[i - 1]);
+//          i--;
 //       }
-
-//       arr[j + 1] = key;
 //    }
 // };
-
-void insertionSort(std::vector<int> &arr)
-{
-   for (std::size_t i = 1; i < arr.size(); i++)
-   {
-      while (i >= 1 && arr[i] < arr[i - 1])
-      {
-         std::swap(arr[i], arr[i - 1]);
-         i--;
-      }
-   }
-};
--- sorting/main.cpp
+++ sorting/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "bubble_sort.cpp"
 #include "insertion_sort.cpp"
-#include "counting_sort.cpp"
 #include <unordered_map>
 
 int main()
 {
    std::vector<int> vec = {8, 2, 1, 9, 8, 1, 2, 5};
 
-   auto it = std::unique(vec.begin(), vec.end());
+   insertionSort(vec);
 
-   vec.erase(it, vec.end());
-   
    for (int num : vec)
    {
       std::cout << num << " ";
