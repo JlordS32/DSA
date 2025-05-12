2e6e6c14e313ff0b5b1e4bb90444f61fccd49e10
JlordS32 <jlordsty128@gmail.com>
2025-05-12 12:31:49 +1000

    fib func

--- dynamic_programming/fib.cpp
+++ dynamic_programming/fib.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
-int main() {
+int recursiveFib(int n) {
+   if (n <= 1) return n;
+   return recursiveFib(n - 1) + recursiveFib(n - 2);
+}
 
-   return;
+int iterativeFib(int n) {
+   if (n <= 1) return n;
+
+   int a = 0;
+   int b = 1;
+   int c = a + b;
+   for (int i = 2; i < n; i++) {
+      a = b;
+      b = c;
+      c = a + b;
+   }
+   return c;
+}
+
+int main() {
+   std::cout << recursiveFib(6) << std::endl;
+   std::cout << iterativeFib(6) << std::endl;
+
+   return 0;
 }
