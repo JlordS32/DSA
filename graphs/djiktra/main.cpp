53296a91e362808d912c29f88dff2d54836b0e90
JlordS32 <jlordsty128@gmail.com>
2025-06-28 20:35:50 +1000

    latest changes

--- graphs/djiktra/main.cpp
+++ graphs/djiktra/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <limits>
 #include <algorithm>
+#include <cmath> 
 #include "../graph.hpp"
 
 const int MAX_INT = std::numeric_limits<int>::max();
+const int width = 3; 
 
 void dijkstras(Graph &g, int source)
 {
@@ -14,7 +16,6 @@ void dijkstras(Graph &g, int source)
    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
    dist[source] = 0;
 
-   int current = source;
    pq.push({0, source}); // distance = 0, node = source
 
    while (!pq.empty())
@@ -39,9 +40,55 @@ void dijkstras(Graph &g, int source)
       std::cout << i << ": " << dist[i] << std::endl;
 }
 
+int heuristic(int a, int b)
+{
+   int ax = a % width, ay = a / width;
+   int bx = b % width, by = b / width;
+   return sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
+}
+
+void astar(Graph &g, int start, int goal)
+{
+   std::vector<int> gScore(g.getVertices(), MAX_INT);
+   std::vector<int> fScore(g.getVertices(), MAX_INT);
+   std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> openSet;
+
+   gScore[start] = 0;
+   fScore[start] = heuristic(start, goal);
+   openSet.push({fScore[start], start});
+
+   while (!openSet.empty())
+   {
+      int current = openSet.top().second;
+      openSet.pop();
+
+      if (current == goal)
+      {
+         std::cout << "Reached goal with cost: " << gScore[goal] << std::endl;
+         return;
+      }
+
+      for (auto edge : g.getAdjList()[current])
+      {
+         int neighbor = edge.first;
+         int weight = edge.second;
+         int tentative_g = gScore[current] + weight;
+
+         if (tentative_g < gScore[neighbor])
+         {
+            gScore[neighbor] = tentative_g;
+            fScore[neighbor] = tentative_g + heuristic(neighbor, goal);
+            openSet.push({fScore[neighbor], neighbor});
+         }
+      }
+   }
+
+   std::cout << "Goal not reachable\n";
+}
+
 int main()
 {
-   Graph g(6); // Undirected by default
+   Graph g(6);
 
    g.addEdge(0, 1, 4);
    g.addEdge(0, 2, 1);
@@ -51,7 +98,7 @@ int main()
    g.addEdge(3, 4, 3);
    g.addEdge(4, 5, 1);
 
-   dijkstras(g, 0);
+   astar(g, 0, 4);
 
    return 0;
 }
